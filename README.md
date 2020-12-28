331e9c71b525233d31e61ef6d48ee3c0c7e8a03d
DerMagischeBier <60985043+DerMagischeBier@users.noreply.github.com>
2020-12-28 23:10:24 +0100

    Enhance image output, change approximated function

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 Fitting a basic neural network to samples of a simple function using the [tch](https://crates.io/crates/tch) crate. The resulting predictions for the training samples are shown in a created plot looking like the one below.
 
 ![Regression plot](regression_plot.png)
+
+The underlying function the neural network is trained to approximate is given by `0.09 * x^3 + 2 * cos(x) - 1`. During the training data creation an artificial sampling error is applied given by normal distributed error fractions.
+
+After checking out the repository and building and running by invoking `cargo run` a comparable image output should be created. Be aware that building can take a while as the `tch` crate will depending on the exact setup download the desired version of C++ PyTorch library (libtorch). 
--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,57 @@
 use anyhow::Result;
 use plotters::prelude::*;
+use rand::distributions::Uniform;
 use rand::{thread_rng, Rng};
+use rand_distr::{Distribution, Normal};
 use tch::{nn, nn::Module, nn::OptimizerConfig, Device, Kind, Reduction, Tensor};
 
 fn net(vs: &nn::Path) -> impl Module {
     nn::seq()
-        .add(nn::linear(vs / "layer1", 1, 10, Default::default()))
+        .add(nn::linear(vs / "layer1", 1, 50, Default::default()))
         .add_fn(|xs| xs.relu())
-        .add(nn::linear(vs, 10, 1, Default::default()))
+        .add(nn::linear(vs, 50, 1, Default::default()))
 }
 
+/// Samples points from function with added error following a normal distribution
 fn generate_data(n_data_points: usize) -> (Vec<f64>, Vec<f64>) {
-    let data = (0..n_data_points)
-        .map(|_| thread_rng().gen_range(-4.0..4.0))
-        .collect::<Vec<f64>>();
+    let data: Vec<f64> = thread_rng()
+        .sample_iter(Uniform::new(-4.0, 4.0))
+        .take(n_data_points)
+        .collect();
 
-    let data_output = data.iter().map(|&x| x * x).collect::<Vec<_>>();
+    // normal distribution to add sampling error
+    let normal = Normal::new(0.0, 1.0).unwrap();
+    let data_output = data
+        .iter()
+        .map(|&x| {
+            let error = normal.sample(&mut thread_rng());
+
+            (0.09 * x.powi(3) + 2.0 * x.cos() - 1.0) + error
+        })
+        .collect::<Vec<_>>();
 
     (data, data_output)
 }
 
 fn main() -> Result<()> {
-    let (input_data, output_data) = generate_data(200);
+    let (input_data, output_data) = generate_data(500);
 
     // create tensors from regression data
     let input = Tensor::of_slice(&input_data)
-        .view((input_data.len() as i64,1))
+        .view((input_data.len() as i64, 1))
         .to_kind(Kind::Float);
     let output = Tensor::of_slice(&output_data)
-        .view((output_data.len() as i64,1))
+        .view((output_data.len() as i64, 1))
         .to_kind(Kind::Float);
 
     let vs = nn::VarStore::new(Device::Cpu);
-    // set up neural net structure
+    // set up neural network structure
     let net = net(&vs.root());
     let mut opt = nn::Adam::default().build(&vs, 5e-2).unwrap();
-    for epoch in 1..500 {
-        let loss = net
-            .forward(&input)
-            .mse_loss(&output, Reduction::Sum);
+
+    // train neural network
+    for epoch in 1..400 {
+        let loss = net.forward(&input).mse_loss(&output, Reduction::Mean);
         opt.backward_step(&loss);
 
         println!("epoch: {:4} train loss: {:8.5}", epoch, f64::from(&loss),);
@@ -48,26 +61,46 @@ fn main() -> Result<()> {
 
     root.fill(&WHITE)?;
 
-    let mut plot = ChartBuilder::on(&root).build_cartesian_2d(-4.5f64..4.5f64, -0.5f64..17f64)?;
+    let mut plot = ChartBuilder::on(&root)
+        .x_label_area_size(40)
+        .y_label_area_size(40)
+        .build_cartesian_2d(-4.5f64..4.5f64, -10f64..6f64)?;
 
-    // plot training data
+    plot.configure_mesh()
+        .light_line_style(&WHITE)
+        .draw()
+        .unwrap();
+
+    // plot training data points
     plot.draw_series(
         input_data
             .iter()
             .zip(output_data.iter())
             .map(|(x, y)| Circle::new((*x, *y), 2, GREEN.filled())),
-    )?;
+    )?
+    .label("Sampled Training Data")
+    .legend(|(x, y)| Circle::new((x + 10, y), 2, GREEN.filled()));
 
     // create points to sample neural net output
-    let sampling_points = (0..400)
-        .map(|x| (x - 200) as f64 * 0.02)
-        .collect::<Vec<_>>();
+    let sampling_points: Vec<f64> = (0..400).map(|x| (x - 200) as f64 * 0.02).collect();
 
-    plot.draw_series(sampling_points.iter().map(|x| {
-        let sampling_point_input = Tensor::of_slice(&[*x]).view((1, 1)).to_kind(Kind::Float);
-        let predicted_result = net.forward(&sampling_point_input);
-        Circle::new((*x, predicted_result.double_value(&[0])), 1, RED.filled())
-    }))?;
+    // plot neural network approximation of underlying function
+    plot.draw_series(LineSeries::new(
+        sampling_points.iter().map(|x| {
+            let sampling_point_input = Tensor::of_slice(&[*x]).view((1, 1)).to_kind(Kind::Float);
+            let predicted_result = net.forward(&sampling_point_input);
+
+            (*x, predicted_result.double_value(&[0]))
+        }),
+        Into::<ShapeStyle>::into(&CYAN).stroke_width(2),
+    ))?
+    .label("Neural Network Approximation")
+    .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &CYAN));
+
+    plot.configure_series_labels()
+        .position(SeriesLabelPosition::LowerRight)
+        .border_style(&BLACK)
+        .draw()?;
 
     Result::Ok(())
 }
